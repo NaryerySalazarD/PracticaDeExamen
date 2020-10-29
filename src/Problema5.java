3e3f7cef618b7e44194c9613de9e07ceb6dc2d0b
gigabyte <gigabyte@DESKTOP-RLNDKFN>
2020-10-28 20:16:40 -0600

    terminado

--- src/Problema5.java
+++ src/Problema5.java
@@ -1,13 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author gigabyte
- */
+import java.util.Scanner;
+
+
+
 public class Problema5 {
-    
+    public static void main( String[] args){
+        Scanner ac=new Scanner (System.in);
+        
+        //pido el dato de entrada
+        System.out.println("Cuántos términos de presición desea: ");
+        int n=ac.nextInt();
+        
+        //Proceso
+        System.out.println("pi= "+pi(n));
+        
+    }
+    public static double pi(int n){
+        int d=1,sig=1;
+        double t,s=0;
+        for(int i=1;i<=n;i++){
+            t=(double)1/d*sig;
+            s+=t;
+            d +=2;
+            sig*=-1;
+        }
+        return s*4;
+    }
 }
--- src/Problema7.java
+++ src/Problema7.java
@@ -1,13 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author gigabyte
- */
+import java.util.Scanner;
+
+
 public class Problema7 {
+    private static Scanner ac=  new Scanner(System.in);
     
+    public static void main(String[] args){
+        //usar el método
+        System.out.println("Ingrese el número de la base: ");
+        int base=ac.nextInt();
+         System.out.println("\nIngrese el número del exponente: ");
+        int exponente=ac.nextInt();
+        System.out.println("El resultado es: "+Potencia(base,exponente));
+    }
+    //método para llegar al resultado de la potencia
+    public static int Potencia(int base,int exponente){
+        if (exponente==0){
+            return 1;
+        } 
+        else{
+            return base*Potencia(base,exponente-1);
+        }
+    }
 }
