e5dd167ec186e2238a0bedd4991d225fc1e4e291
gigabyte <gigabyte@DESKTOP-RLNDKFN>
2020-11-03 04:51:31 -0600

    terminado.

--- src/Problema3.java
+++ src/Problema3.java
@@ -2,47 +2,66 @@
 import java.util.Scanner;
 
 public class Problema3 {
-//Sin terminar
-    private static int[] dependiente(int[] cosa) {
-        int salarioMensual;
-        int dependiente;
+
+    private static double calcularImpuesto(int dependiente, double salarioMensual) {
+        double salarioGravable = 0;
+        double montoImpuesto = 0;
         Scanner entrada = new Scanner(System.in);
         System.out.print("Introduce el número de dependientes: ");
         dependiente = entrada.nextInt();
         Scanner entrada1 = new Scanner(System.in);
         System.out.print("Introduce tu salario: ");
         salarioMensual = entrada1.nextInt();
+        switch (dependiente) {
+            case 1:
+                System.out.println("\nSu salario es gravable en un 100%.");
+                salarioGravable = salarioMensual;
+                break;
+            case 2:
+                System.out.println("\nSu salario es gravable en un 90%.");
+                salarioGravable = salarioMensual * 0.9;
+                break;
+            case 3:
+                System.out.println("\nSu salario es gravable en un 70%.");
+                salarioGravable = salarioMensual * 0.7;
+                break;
+            case 4:
+                System.out.println("\nSu salario es gravable en un 60%.");
+                salarioGravable = salarioMensual * 0.6;
+                break;
+            default:
+                System.out.println("\nSu salario es gravable en un 40%.");
+                salarioGravable = salarioMensual * 0.4;
+                break;
+        }
 
-        if (dependiente == 1) {
-            System.out.println("\nSu impuesto sobre la renta es gravable en un 100%.");
+        if (salarioGravable < 200000) {
+            System.out.println("\nSu impuesto sobre la renta es de un 0% sobre su salario gravable, el cual es de " + salarioGravable);
+            montoImpuesto = salarioGravable * 0;
+        } else if (salarioGravable < 400000) {
+            System.out.println("\nSu impuesto sobre la renta es de un 10% sobre su salario gravable, el cual es de " + salarioGravable);
+            montoImpuesto = salarioGravable * 0.10;
+        } else if (salarioGravable < 600000) {
+            System.out.println("\nSu impuesto sobre la renta es de un 15% sobre su salario gravable, el cual es de " + salarioGravable);
+            montoImpuesto = salarioGravable * 0.15;
+        } else if (salarioGravable < 800000) {
+            System.out.println("\nSu impuesto sobre la renta es de un 20% sobre su salario gravable, el cual es de " + salarioGravable);
+            montoImpuesto = salarioGravable * 0.20;
+        } else  {
+            System.out.println("\nSu impuesto sobre la renta es de un 30% sobre su salario gravable, el cual es de " + salarioGravable);
+            montoImpuesto = salarioGravable * 0.30;
         }
-        if (dependiente == 2) {
-            System.out.println("\nSu impuesto sobre la renta es gravable en un 90%.");
-        }
-        if (dependiente == 3) {
-            System.out.println("\nSu impuesto sobre la renta es gravable en un 70%.");
-        }
-        if (dependiente == 4) {
-            System.out.println("\nSu impuesto sobre la renta es gravable en un 60%.");
-        }
-        if (dependiente > 4) {
-            System.out.println("\nSu impuesto sobre la renta es gravable en un 40%.");
-        }
-         if (salarioMensual<200000) {
-            System.out.println("\nSu impuesto sobre la renta es de un 0%.");
-        }
-          if (salarioMensual<=400000) {
-            System.out.println("\nSu impuesto sobre la renta es de un 10%.");
-        }
-           if (salarioMensual<200000) {
-            System.out.println("\nSu impuesto sobre la renta es de un 0%.");
-        }
-        
-        return cosa;
+
+        return montoImpuesto;
     }
 
     public static void main(String[] args) {
-        
+  int dependiente=0;
+  double salarioMensual=0;
+        //captura lo que el método retorna
+        double resultadoImpuesto;
+        resultadoImpuesto = calcularImpuesto(dependiente, salarioMensual);
+        System.out.print("El monto a pagar es de: "+resultadoImpuesto);
 
     }
 }
