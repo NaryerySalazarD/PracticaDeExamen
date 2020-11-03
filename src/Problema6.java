966edfb3a138fdc74bc22cbbbf884101e1f9acd3
gigabyte <gigabyte@DESKTOP-RLNDKFN>
2020-11-03 02:39:42 -0600

    terminado.

--- src/Problema6.java
+++ src/Problema6.java
@@ -1,13 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author gigabyte
- */
+import java.util.Scanner;
+
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+
+        String figura;
+        
+
+        double n;
+
+        double largo;
+        double ancho;
+
+        double pi = 3.1416;
+        double r;
+        while (true) {
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("\nIndique el nombre de la figura a la cuál le desea sacar el área: ");
+            figura = entrada.next();
+            switch (figura) {
+                case "cuadrado": {
+                    Scanner entrada1 = new Scanner(System.in);
+                    System.out.print("Introduce la medida de uno de los lados del cuadrado: ");
+                    n = entrada1.nextDouble();
+                    System.out.print("El resultado del área del cuadrado es: ");
+                    System.out.print(n * 4);
+                    break;
+                }
+
+                case "rectangulo": {
+                    Scanner entrada2 = new Scanner(System.in);
+                    System.out.print("Introduce la medida del largo del rectánculo: ");
+                    largo = entrada2.nextDouble();
+                    Scanner entrada3 = new Scanner(System.in);
+                    System.out.print("Introduce la medida del ancho del rectángulo: ");
+                    ancho = entrada3.nextDouble();
+                    System.out.print("El resultado del área del rectángulo es: ");
+                    System.out.print(largo * ancho);
+                    break;
+                }
+                case "circulo": {
+                    Scanner entrada4 = new Scanner(System.in);
+                    System.out.print("Introduce la medida del radio de tu círculo: ");
+                    r = entrada4.nextDouble();
+                    System.out.print("El resultado del área del círculo es: ");
+                    System.out.print(pi * (r * r));
+                    break;
+                }
+
+            }
+
+        }
+    }
 }
