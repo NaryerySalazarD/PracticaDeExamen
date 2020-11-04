740187abcf32127ad2b97ee76bb399258899d954
gigabyte <gigabyte@DESKTOP-RLNDKFN>
2020-11-04 09:21:32 -0600

    terminado.

--- src/CompuProgramas.java
+++ src/CompuProgramas.java
@@ -1,13 +1,178 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author gigabyte
- */
 public class CompuProgramas {
-    
+
+    public static void main(String[] args) {
+        //primer compugrama
+        int n = 5;
+        for (int i = 0; i <= n - n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print("-");
+            }
+            for (int k = 5; k == n; k++) {
+                System.out.print(">");
+
+            }
+        }
+        System.out.println();
+        //segundo compugrama
+        int m = 5;
+        for (int i = 0; i < m - 2; i++) {
+            for (int j = 0; j < m - 1; j++) {
+                System.out.print("-");
+            }
+            for (int k = 5; k == m; k++) {
+                System.out.print(">");
+            }
+            System.out.println();
+        }
+        //tercer compugrama
+        int l = 5;
+        //línea superior
+        for (int i = 0; i < l; i++) {
+            System.out.print("*");
+
+        }
+          System.out.println();
+          //centro
+          for(int i=0;i<n-2;i++){
+               System.out.print("*");
+               for(int j=0;j<n-2;j++){
+                    System.out.print(" ");
+               }
+                 System.out.println("*");
+          }
+          
+          //línea inferior
+          for (int i = 0; i < l; i++) {
+            System.out.print("*");
+
+        }
+             System.out.println();
+          //cuarto compugrama
+           //línea superior
+        for (int i = 0; i < l; i++) {
+            System.out.print("*");
+
+        }
+          System.out.println();
+           //centro1
+          for(int i=0;i<n-4;i++){
+               System.out.print("*");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("*");
+                     System.out.print("  ");
+               }
+                 System.out.println("*");
+          }
+           //centro2
+          for(int i=0;i<n-4;i++){
+               System.out.print("* ");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("*");
+                     System.out.print(" *");
+               }
+                 System.out.println();
+          }
+            //centro3
+          for(int i=0;i<n-4;i++){
+               System.out.print("*");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("  ");
+                     System.out.print("*");
+               }
+                 System.out.println("*");
+          }
+           //línea inferior
+          for (int i = 0; i < l; i++) {
+            System.out.print("*");
+
+        }
+           System.out.println();
+           
+           //quinto compugrama
+           //línea superior
+        for (int i = 0; i < l; i++) {
+            System.out.print("*");
+
+        }
+        System.out.println();
+        //centro1
+          for(int i=0;i<n-4;i++){
+               System.out.print("*");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("  ");
+                     System.out.print("*");
+               }
+                 System.out.println("*");
+          }
+          //centro2
+          for(int i=0;i<n-4;i++){
+               System.out.print("* ");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("*");
+                     System.out.print(" *");
+               }
+                 System.out.println();
+          }
+            //centro3
+          for(int i=0;i<n-4;i++){
+               System.out.print("*");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("*");
+                     System.out.print("  ");
+               }
+                 System.out.println("*");
+          }
+        
+         //línea inferior
+          for (int i = 0; i < l; i++) {
+            System.out.print("*");
+
+        }
+           System.out.println();
+           //sexto compugrama
+           //línea superior
+        for (int i = 0; i < l; i++) {
+            System.out.print("*");
+
+        }
+        System.out.println();
+        //centro1
+          for(int i=0;i<n-4;i++){
+               System.out.print("*");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("*");
+                     System.out.print(" ");
+                     System.out.print("*");
+               }
+                 System.out.println("*");
+          }
+        //centro2
+          for(int i=0;i<n-4;i++){
+               System.out.print("* ");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("*");
+                     System.out.print(" *");
+               }
+                 System.out.println();
+          }
+           //centro3
+          for(int i=0;i<n-4;i++){
+               System.out.print("*");
+               for(int j=0;j<n-4;j++){
+                    System.out.print("*");
+                     System.out.print(" ");
+                     System.out.print("*");
+               }
+                 System.out.println("*");
+          }
+       
+        //línea inferior
+          for (int i = 0; i < l; i++) {
+            System.out.print("*");
+
+        }
+    }
 }
+
+
