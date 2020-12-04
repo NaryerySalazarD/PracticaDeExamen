f70d2b92b414ab55174bd3b1f96d604f4bf0a731
gigabyte <gigabyte@DESKTOP-RLNDKFN>
2020-12-04 07:47:02 -0600

    Problema2 proyecto.(avance)

---
+++ src/IIProyecto/Problema2/MaqConvertidora.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IIProyecto.Problema2;
+
+/**
+ *
+ * @author gigabyte
+ */
+public class MaqConvertidora {
+    Moneda monedaColon;
+    Moneda monedaGene1;
+    Moneda monedaGene2;
+            
+    
+    public MaqConvertidora(){
+    }
+
+    public MaqConvertidora(Moneda monedaColon, Moneda monedaGene1, Moneda monedaGene2) {
+        this.monedaColon = monedaColon;
+        this.monedaGene1 = monedaGene1;
+        this.monedaGene2 = monedaGene2;
+    }
+
+    
+    
+    
+    //Este metodo requiere que monedaOrigen y monedaSalida esten establecidas
+    public float convertir(Moneda monedaOrigen, Moneda monedaSalida, float cantidadOrigen){
+        float resultado = 0;
+        if(monedaOrigen.getNombre().equals("colon")){
+               if(monedaSalida.getNombre().equals("colon")){
+                   resultado = cantidadOrigen;
+               }
+               else if(monedaSalida.getNombre().equals(monedaGene1.getNombre())){
+                   resultado = cantidadOrigen/monedaGene1.getValorColon();
+               }
+               else{
+                   resultado = cantidadOrigen/monedaGene2.getValorColon();
+               }
+           }
+           else if(monedaOrigen.getNombre().equals("Dolar")){
+           
+           }
+           else {
+           
+           }
+        
+        return resultado;}
+    
+    public Moneda getMonedaColon() {
+        return monedaColon;
+    }
+}
+    
+    
---
+++ src/IIProyecto/Problema2/Tester.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IIProyecto.Problema2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gigabyte
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+
+        //CONFIGURAR MONEDAS
+        Moneda mColon = new Moneda("Colon", 1);
+        Moneda mGen1 = new Moneda();
+        Moneda mGen2 = new Moneda();
+
+        //INICIA 
+        Scanner s = new Scanner(System.in);
+        
+        
+
+        while (true) {
+            System.out.println("Digite sus otras dos monedas");
+            mGen1.setNombre(s.next());
+            mGen2.setNombre(s.next());
+            System.out.println("Digite el valor de la tasa de su moneda de origen");
+            mGen1.setValorColon(s.nextFloat());
+            mGen2.setValorColon(s.nextFloat());
+            MaqConvertidora maq = new MaqConvertidora(mColon, mGen1, mGen2);
+            System.out.println("Digite la moneda de origen 1=Colon 2="+mGen1.getNombre()+"3="+mGen2.getNombre());
+            int numMonedaOrigen = s.nextInt();
+            System.out.println("Digite la moneda de salida 1=Colon 2="+mGen1.getNombre()+"3="+mGen2.getNombre());
+            int numMonedaSalida = s.nextInt();
+            System.out.println("Digite la cantidad");
+            int cantidad = s.nextInt();
+            
+
+            if(numMonedaOrigen==1){//Colon
+                if(numMonedaSalida==1){//Colon
+                    System.out.println("El valor equivalente es: "+maq.convertir(mColon, mColon, cantidad));
+                }
+                else if(numMonedaSalida==2){ //Dolar
+                    System.out.println("El valor equivalente es: "+maq.convertir(mColon, mGen1, cantidad)); 
+                }
+                else { //euro
+                    System.out.println("El valor equivalente es: "+maq.convertir(mColon, mGen2, cantidad)); 
+                }
+            }
+            else if(numMonedaOrigen==2){//Dolar
+
+            }
+            else{ //Euro
+
+            }
+        }
+    }
+}
